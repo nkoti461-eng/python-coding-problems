dc5f8d15f67fe6a03f3855166b5e4196d7f2dc51
nkoti461-eng <nkoti461@gmail.com>
2026-02-24 22:40:46 +0530

    Add files via upload
    This problem helps in understanding Python arithmetic operators and how they behave with integers and floating-point numbers.

---
+++ Day_1.operators(Artihmetic operators).py
@@ -0,0 +1,19 @@
+# Takes two integer inputs from the user.
+# Performs the following arithmetic operations:
+# Addition
+# Subtraction
+# Multiplication
+# Division
+# Floor Division
+# Modulus
+# Exponentiation
+# Displays the result of each operation clearly.
+a=5
+b=6
+print(f"Arithmatic oper of Addition:",a+b)
+print(f"Arithmatic oper of Subtraction:",a-b)
+print(f"Arithmatic oper of Multiplication:",a*b)
+print(f"Arithmatic oper of Division:",a/b)
+print(f"Arithmatic oper of floorDivision:",a//b)
+print(f"Arithmatic oper of Modulus:",a%b)
+print(f"Arithmatic oper of Exponetiation:",a**b)
