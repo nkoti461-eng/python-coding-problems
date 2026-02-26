5073100509128cb95e7280eab68dcb9e9d55b40a
nkoti461-eng <nkoti461@gmail.com>
2026-02-26 22:32:37 +0530

    Add files via upload
    This project demonstrates the implementation of Assignment Operators and Comparison Operators in Python.
    The program performs step-by-step operations on variables and prints the updated results after each operation. It also explains datatype behavior and operator functionality.

---
+++ Day_3.operators(Assignment and comparison operators).py
@@ -0,0 +1,60 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Assignment Operation
+# Write a Python program that:
+# Initializes a variable a = 5.
+# Performs all the following assignment operations step-by-step:
+# += 3
+# -= 3
+# *= 3
+# /= 3
+# %= 3
+# //= 3
+# **= 3
+# &=
+# ^=
+# >>=
+# <<=
+# After each operation, print the updated value of a.
+# Finally, print:
+# The final value of a
+# The datatype of a
+... a = 5
+... print(f"Assignment oper of Assignment Operator: {a}")
+... a += 3
+... print(f"Assignment oper of Addition Assignment: {a}")
+... a -= 3
+... print(f"Assignment oper of Subtraction Assignment: {a}")
+... a *= 3
+... print(f"Assignment oper of Multiplication Assignment: {a}")
+... a /= 3
+... print(f"Assignment oper of Division Assignment: {a}")
+... a %= 3
+... print(f"Assignment oper of Modulus Assignment: {a}")
+... a //= 3
+... print(f"Assignment oper of Floor Division Assignment: {a}")
+... a **= 3
+... print(f"Assignment oper of Exponentiation Assignment: {a}")
+... # &= : unsupported operand type for 'float' and 'int'
+... # ^= : unsupported operand type for 'float' and 'int'
+... # >>= : unsupported operand type for 'float' and 'int'
+... # <<= :unsupported operand type for 'float' and 'int'
+... 
+... #Comparison Operation
+... # Write a Python program that:
+... # 1:Takes two numbers as input from the user.
+... # 2:Compares both numbers using:
+... # >Equal to
+... # >Not equal to
+... # >Greater than
+... # >Less than
+... # >Greater than or equal to
+... # >Less than or equal to
+... # 3:Prints all results clearly.
+... a=5
+... b=6
+... print(f"Comparison Operation of Equal:",a==b)
+... print(f"Comparison Operation of not Equal:",a!=b)
+... print(f"Comparison Operation of Greater than:",a>b)
+... print(f"Comparison Operation of Less than:",a<b)
+... print(f"Comparison Operation of Greater than or equal to:",a>=b)
