b6009e4de289d5469d1e70a9f84a5276f127077f
Koteswarararao Vulli <nkoti461@gmail.com>
2026-03-04 21:31:40 +0530

    Add files via upload

---
+++ Day_8_List and List(Building Functions).py
@@ -0,0 +1,39 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> list:list is used to store the multiple items in the list.
+... >list is mutable.
+... >in the order.
+... >in the list we use nested list means list inside the another list.
+... >it allow the dulpicates.
+... a=[]
+... b=type(a)
+... a
+... a=[50,30,"sandeep"]
+... a[:]#silicing:in the list we use silicing also.
+... building function of list
+... a=[1,2,3,4,5]
+... a.append(34)#adding one element only
+... print(a)
+... a.extend([34,345])#adding multiple element at a time
+... print(a)
+... a.pop()#it remove this right most value
+... print(a)
+... del()#it will delete the whole  list
+... print(a)
+... a=[11,12,13,[123,134,10]]
+... a.copy()#it id copy and clear the list
+... print(a)
+... a=[12,13,14,1245,1234,1]
+... a.clear()
+... print(a)
+... a.count(12)
+... print(a)
+... a.sort()#it give in ascending order
+... print(a)
+... a.sort(reverse=True)# it gives in decending order
+... a.index(2)# it gives first index of the given number
+... print(a)
+... a.reverse()#it gives in reverse order
+... print(a)
+... a.insert(1,123456)#here we have to give position to insert a value
+... print(a) 
