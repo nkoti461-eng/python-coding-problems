fb2a3e455713efc9f1dbb39a2426072050c7e48b
Koteswarararao Vulli <nkoti461@gmail.com>
2026-03-06 11:12:05 +0530

    Add files via upload

---
+++ Day_9_List By using len.py
@@ -0,0 +1,7 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Take a string from the user and convert in to tuple print the character and length of tuple.
+... s=input()
+... t=tuple(s)
+... for i in t:
+...     print(i)
