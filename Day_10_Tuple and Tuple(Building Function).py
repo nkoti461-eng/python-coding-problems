ba0ced3da0afff17dacaa4d70ef15fef05394d58
Koteswarararao Vulli <nkoti461@gmail.com>
2026-03-09 22:14:16 +0530

    Add files via upload

---
+++ Day_10_Tuple and Tuple(Building Function).py
@@ -0,0 +1,27 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> tuple:
+... >tuple is order data.
+... >it is heterogenous.
+... >it allow the duplications.
+... >it is immutable collection of element in  python.
+... >it is similar to list but the main different is tuple cannot be change after creation
+... a=()#tuple:heterogeous,order,mutable,nested list,duplicate
+... type(a)
+... # a,b,c=(90,23,22)#un packing
+... # print(a)
+... a,b,c=("sa","so","ma")
+... a,b,c=12,12,13# packing
+... print(a,b,c)
+... a=([1,2.3,4+8j],[2.4,34])#immutable
+... a
+... #build in functions
+... t=(1,2,3,4,5)
+... t.count(1)
+... print(t)
+... print(t.index(4))
+... print(len(t))
+... print(min(t))
+... print(max(t))
+... print(sum(t))
+... t
