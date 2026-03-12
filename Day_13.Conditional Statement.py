f0fb68c4bf532c0e8c98456fa996255f2205b36a
Koteswarararao Vulli <nkoti461@gmail.com>
2026-03-12 22:43:33 +0530

    Add files via upload

---
+++ Day_13.Conditional Statement.py
@@ -0,0 +1,41 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Conditional Statement:
+... Conditional statements allow a program to make decisions based on conditions.
+... They execute different blocks of code depending on whether a condition is True or False.
+... >in this conditional statement there are 3types:
+... 1.if
+... 2.else
+... 3.if-elif-elif
+... 4.Nested if
+... 1. if Statement:
+... The if statement executes a block of code only when the condition is true.
+... if condition:
+...     statement
+... 2. if-else Statement:
+... The if-else statement executes one block if the condition is true and another if it is false.
+... if condition:
+...     statement1
+... else:
+...     statement2
+... 3. if-elif-else Statement
+... Used when there are multiple conditions to check.
+... if condition1:
+...     statement1
+... elif condition2:
+...     statement2
+... else:
+...     statement3
+... 4. Nested if Statement
+... An if statement inside another if statement.
+... age = 20
+... citizen = True
+... if age >= 18:
+...     if citizen:
+...         print("Eligible to vote")
+... >in the Conditions usually use comparison operators:
+... 1.== Equal
+2.!= Not equal
+3.> Greater than
+4.< Less than
+4.>= Greater than or equal
