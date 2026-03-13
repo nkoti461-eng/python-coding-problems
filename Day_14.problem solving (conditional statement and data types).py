131af6f97abc23b8d2671fc8d21f7195f6b1fc49
Koteswarararao Vulli <nkoti461@gmail.com>
2026-03-13 23:27:42 +0530

    Add files via upload

---
+++ Day_14.problem solving (conditional statement and data types).py
@@ -0,0 +1,14 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Write a Python program that:
+... # Takes a number from the user
+... # Checks whether the number is
+... # Positive
+... # Negative
+... # Zero
+... n=int(input("enter the value"))
+... if n<0:
+...     print("it is negative value")
+... elif n>0:
+...     print("it is positive value")
+... else:
