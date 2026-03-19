232ff02680cbb6c9edfd8717cc840563b6504995
Koteswarararao Vulli <nkoti461@gmail.com>
2026-03-19 21:53:33 +0530

    Add files via upload

---
+++ Day_18.problem sloving.py
@@ -0,0 +1,8 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Design a Python program to validate whether a given email address is valid based 
+... # on standard email format rules.
+... if '@'in email:
+...     if email.endswith('.com'):
+...         print("the email is correct")
+...     else:
