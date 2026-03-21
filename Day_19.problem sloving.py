27f179c797bf3c382aceb74405a9cfc096b7c20b
Koteswarararao Vulli <nkoti461@gmail.com>
2026-03-21 22:48:42 +0530

    Add files via upload

---
+++ Day_19.problem sloving.py
@@ -0,0 +1,8 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Write a Python program that simulates a simple word guessing game. The program should have a predefined word, and the user must guess 
+... # the correct word.
+... print("lets play a game:")
+... my_word='pipe'
+... your_word=input('guss a word')
+... if my_word==your_word:
