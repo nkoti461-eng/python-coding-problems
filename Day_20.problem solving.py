01cc01d2bbf8d703ca5d1d2429d81e3d5cdad88c
Koteswarararao Vulli <nkoti461@gmail.com>
2026-03-25 20:55:40 +0530

    Add files via upload

---
+++ Day_20.problem solving.py
@@ -0,0 +1,8 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Write a Python program that takes user input as a list containing a name 
+... (string) and age (integer).
+... info=eval(input("enter the name and age"))
+... if 'a' in info[0] or 'e' in info[0] or 'i' in info[0] or 'o' in info[0] or 'u' in info[0]:
+...         print("name is in vowel")
+... if info[1]>20 and info[1]<50:
