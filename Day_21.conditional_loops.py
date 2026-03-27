a52690ea2e8b79fd9fa37a117625d44ee98b41b1
Koteswarararao Vulli <nkoti461@gmail.com>
2026-03-27 20:45:29 +0530

    Add files via upload

---
+++ Day_21.conditional_loops.py
@@ -0,0 +1,17 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Python Loops
+... Python has two primitive loop commands:
+... while loops
+... for loops
+... #The while Loop:
+... the while loop we can execute a set of statements as long as a condition is true.
+... Ex:i = 1
+... while i < 6:
+...   print(i)
+...   i += 1
+... #The For loop:
+... A for loop is a control flow statement that allows you to execute a block of code repeatedly based on a specified condition, commonly used for
+... iterating over sequences or performing tasks a fixed number of times.
+... Ex:fruits = ["apple", "banana", "cherry"]
+... for x in fruits:
